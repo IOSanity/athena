f561b9f02abe416a442df9ba481f039f4a8aa46d
Angel Fumero Revetti <katu.bsb@gmail.com>
2017-03-26 21:29:49 +0100

    - Change tests to more readable testing file

--- test/decoders/json.test.js
+++ test/decoders/json.test.js
@@ -1,12 +1,10 @@
 import decoder from '../../src/decoders';
 
-let assert = require('chai').assert;
-
 
 describe('JSONDecoder', () => {
-    it('should return an object', ()=> {
+    test('should return an object', ()=> {
 
         let jsonStringed = '{"key":"value"}';
-        assert.isObject(decoder.json(jsonStringed))
+        expect(typeof decoder.json(jsonStringed)).toBe('object')
     });
 });
