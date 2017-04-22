d2f1cf9bf5048b141f25c2028894911728c30d0e
Angel Fumero Revetti <katu.bsb@gmail.com>
2017-04-22 14:10:46 +0100

    - Added try catchs on server

--- test/WebSocketServer.test.js
+++ test/WebSocketServer.test.js
@@ -7,22 +7,25 @@ describe('WebSocketServer', () => {
     const uuidV4RegExp = /[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}/i;
     const closeCb = jest.fn(() => WebSocketConnection.connection = false);
     const messageCb = jest.fn((message, id) => {});
+    const errorCb = jest.fn(() =>  WebSocketConnection.connection = false);
     const message = 'Message sent from Client';
     const serverMessage = 'Message sent from WebSocketServer';
     const closeMessage = 'Closed by client';
 
     const wssConfiguration = {
         port: 8080,
-        hostname: 'localhost',
+        hostname: '127.0.0.1',
         messageCb: messageCb,
-        closeCb: closeCb
+        closeCb: closeCb,
+        errorCb: errorCb
     };
 
     const wsServer = new WebSocketServer(
         wssConfiguration.hostname,
         wssConfiguration.port,
         wssConfiguration.messageCb,
-        wssConfiguration.closeCb);
+        wssConfiguration.closeCb,
+        wssConfiguration.errorCb);
 
     let WebSocketConnection, wsServerClient;
 
@@ -30,12 +33,11 @@ describe('WebSocketServer', () => {
 
 
     beforeEach((done)=>{
-        jasmine.DEFAULT_TIMEOUT_INTERVAL=5000;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL=7000;
         closeCb.mockClear();
         messageCb.mockClear();
         WebSocketConnection = new WebSocketConnectionHelper();
         wsClient = new WebSocket('ws://' + wssConfiguration.hostname  + ":" + wssConfiguration.port);
-        wsClient.on('error',(er)=>console.log(er));
         wsClient.on('open', ()=>{
             WebSocketConnection.connection = true;
             wsServerClient = Object.keys(wsServer.clients).map((client) => wsServer.clients[client]).reverse()[0];
@@ -86,12 +88,26 @@ describe('WebSocketServer', () => {
             test('Connection closed from client should remove client from server clients list', async() =>{
                 wsClient.close(1000,closeMessage);
 
-                await WebSocketConnection.isDisconnected()
-                    .then(()=> {
-                        expect(closeCb).toBeCalledWith(closeMessage, wsServerClient.id);
-                        expect(wsServer.clients).not.toContainEqual(wsServerClient.id)
-                    });
+                await WebSocketConnection.isDisconnected();
+
+                expect(closeCb).toBeCalledWith(wsServerClient.id);
+                expect(wsServer.clients).not.toContainEqual(wsServerClient.id)
             });
+
+            test('Connection error must delete client from server clients list and send message with error', async() =>{
+
+                wsClient = new WebSocket('ws://' + wssConfiguration.hostname  + ":" + wssConfiguration.port, {
+                    headers : {
+                        origin: ""
+                    }
+                })
+
+                wsClient.on('error', (error)=>{console.log(error)})
+                await WebSocketConnection.isDisconnected();
+
+                expect(errorCb).toBeCalledWith(wsServerClient.id);
+                expect(wsServer.clients).not.toContainEqual(wsServerClient.id)
+            })
         });
 
         describe('WebSocket Server send message',()=>{
@@ -100,8 +116,9 @@ describe('WebSocketServer', () => {
 
                 wsServer.send(serverMessage, wsServerClient.id, spyWebSocketServerSend);
 
-                await WebSocketConnection.receiveMessage()
-                    .then(() => expect(spyWebSocketServerSend).toBeCalled());
+                await WebSocketConnection.receiveMessage();
+
+                expect(spyWebSocketServerSend).toBeCalled();
             })
 
         })
